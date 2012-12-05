0419b2a63813dfa4ff3ee5a2a2b414d21de6ffe9
cjwebb <colinjameswebb@hotmail.com>
2012-12-05 09:45:29 +0000

    adding very simple test to check git & nosetests

---
+++ dynash/test/dynash_tests.py
@@ -0,0 +1,9 @@
+import unittest
+
+'''
+Simple test to check setup of git + nosetests
+'''
+class DynashTests(unittest.TestCase):
+    
+    def test_assert_true(self):
+        self.assertTrue(True)
